a6626227f2118cbea0f941c4fb58b878236ab221
Jan Bohuněk <47471981+honzik2607@users.noreply.github.com>
2023-11-14 11:03:19 +0100

    Update kniha.php

--- MySQL/kniha/kniha.php
+++ MySQL/kniha/kniha.php
@@ -1,6 +1,9 @@
 <html>
     <head>
-
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="https://kappa.lol/QSbKC">
+        <title>Knižní databáze</title>
     </head>
     <body>
         
